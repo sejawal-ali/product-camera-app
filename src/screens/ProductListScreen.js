234e55a0fd35c47ba23d9f24fd55621eb237a3f0
Sejawal Mehmood <149600118+Sejawal110@users.noreply.github.com>
2025-05-14 15:05:06 +0500

    Initial setup with navigation and folder structure

---
+++ src/screens/ProductListScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ProductListScreen = () => {
+  return (
+    <View>
+      <Text>ProductListScreen</Text>
+    </View>
+  )
+}
+
+export default ProductListScreen
+
+const styles = StyleSheet.create({})
